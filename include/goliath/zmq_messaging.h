bc8d1d927ba5422981654ae446bffc747ae952b0
Amorandron <jan@janwilts.com>
2018-05-18 11:50:34 +0200

    Intial commit

---
+++ include/goliath/zmq_messaging.h
@@ -0,0 +1,16 @@
+#pragma once
+
+/**
+ * @file zmq_messaging.h
+ * @author Group 7 - Informatica
+ */
+
+
+/**
+ * @namespace goliath::messaging
+ * @brief Module for receiving and sending messages
+ */
+
+#include "../../src/zmq_io.h"
+#include "../../src/zmq_publisher.h"
+#include "../../src/zmq_subscriber.h"
